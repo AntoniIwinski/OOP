08cc261df0e2c25cb5bd69875186daf1d236e392
MaciejDziembowski <maciej.dziembowski@uczen.zsk.poznan.pl>
2024-10-29 11:12:53 +0100

    class car

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Basic
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+Group g1 = new Group("5c", 20, "Obsti",new string[20]);
+Group g2 = new Group("5b", 23, "Radar",new string[23]);
+
+enum GameChoices
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    PAPER, ROCK, SCISSORS 
 }
+class Group(string name,int count,string mainTeacher, Array students)
+{
+    public string Name = name;
+    public int Count = count;
+    public string MainTeacher = mainTeacher;
+    public Array Students = students;
+
+    
+     
+}
+
---
+++ excercise_Basics/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace excercise_Basics;
+
+using System;
+
+public class Car
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int ProductionDate { get; set; }
+
+    public Car(string brand, string model, int productiondate)
+    {
+        Brand = brand;
+        Model = model;
+        ProductionDate = productiondate;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Brand: {Brand}");
+        Console.WriteLine($"Model: {Model}");
+        Console.WriteLine($"Production date: {ProductionDate}");
+    }
+}
